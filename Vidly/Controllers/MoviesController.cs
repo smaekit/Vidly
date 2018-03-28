a27b9a0ef7099d134d2f833920533dcba4e502c2
smaekit <Marcus.Jakobsson@live.se>
2018-03-29 02:39:53 +0800

    added routing to moviescontroller, by adding attribute routing with regex for the year and month values.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,10 +10,16 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        public ActionResult Random() //ActionsResult can be one of View(),PartialView(),Content(),Redirect(),Json(),File()
         {
             var movie = new Movie() {Name = "Shrek!"};
             return View(movie);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")] //Using attribute routes which are new n better!
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
